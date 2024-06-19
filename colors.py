062a994497cb3024cee5d2cd272e30921bbfd20e
c1vd <98634526+c1vd@users.noreply.github.com>
2024-06-19 16:14:21 +0000

    Delete colors.py

--- colors.py
+++
@@ -1,9 +0,0 @@
-import pyMeow as pm
-
-
-class Colors:
-    orange = pm.get_color("orange")
-    black = pm.get_color("black")
-    cyan = pm.get_color("cyan")
-    white = pm.get_color("white")
-    grey = pm.fade_color(pm.get_color("#242625"), 0.7)
